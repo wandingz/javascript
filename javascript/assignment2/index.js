e673847a7392464eb98d54eb12a98b99ba3274f6
zhaoyi <genius.zhaoyi@gmail.com>
2018-07-27 19:18:32 -0400

    assignment html

---
+++ javascript/assignment2/index.js
@@ -0,0 +1,40 @@
+var status = 'pause';
+
+var leftText = document.getElementById('leftText');
+var rightText = document.getElementById('rightText');
+
+var timeMs = 1000;
+
+function timer() {
+    if (status === 'shiftLeft') {
+        if(leftText.value.length !== 0) {
+            var t = leftText.value[0];
+            leftText.value = leftText.value.slice(1);
+            rightText.value += t;
+        }
+    }
+    if (status === 'shiftRight') {
+        if(rightText.value.length !== 0) {
+            var t = rightText.value[rightText.value.length - 1];
+            rightText.value = rightText.value.slice(0, rightText.value.length - 1);
+            leftText.value = t + leftText.value;
+        }
+    }
+
+    setTimeout(timer, timeMs);
+}
+
+timer();
+
+function shift(s) {
+    status = s;
+}
+
+function pause() {
+    var pause = 'pause';
+    if(status.startsWith(pause)){
+        status = status.slice(pause.length)
+    }else {
+        status = pause + status;
+    }
+}
