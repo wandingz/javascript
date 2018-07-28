d29c7bee1a74cc9f7818499f023abcce1036decb
zhaoyi <genius.zhaoyi@gmail.com>
2018-07-28 17:09:36 -0400

    javascript assignment 2

--- javascript/assignment2/index.js
+++ javascript/assignment2/index.js
@@ -1,40 +1,79 @@
-var status = 'pause';
+// localStorage = window.localStorage;
 
-var leftText = document.getElementById('leftText');
-var rightText = document.getElementById('rightText');
+var form_Attrs = ['name', 'pswd', 'first_name', 'last_name', 'email', 'gender', 'location'];
 
-var timeMs = 1000;
-
-function timer() {
-    if (status === 'shiftLeft') {
-        if(leftText.value.length !== 0) {
-            var t = leftText.value[0];
-            leftText.value = leftText.value.slice(1);
-            rightText.value += t;
-        }
-    }
-    if (status === 'shiftRight') {
-        if(rightText.value.length !== 0) {
-            var t = rightText.value[rightText.value.length - 1];
-            rightText.value = rightText.value.slice(0, rightText.value.length - 1);
-            leftText.value = t + leftText.value;
-        }
-    }
-
-    setTimeout(timer, timeMs);
+function appendtable(User) {
+    var tr = document.createElement("tr");
+    form_Attrs.forEach(a => {
+        var td = document.createElement("td");
+        td.textContent = User[a];
+        tr.appendChild(td);
+    });
+    document.getElementById('myTable').appendChild(tr);
 }
 
-timer();
+var data = JSON.parse(localStorage.getItem('data') || "[]");
+// if (!data) {
+//     data = []
+//     localStorage.setItem('data', JSON.stringify(data));
+// }
+data.forEach(d => appendtable(d));
 
-function shift(s) {
-    status = s;
+document.getElementById("myForm").addEventListener("submit", formSubmit);
+
+var validText = {}
+
+function showValidation(attr, v) {
+    if (!v && !validText[attr]) {
+        validText[attr] = true;
+
+        var parent = document.getElementById(attr);
+
+        var p = document.createElement("div");
+        p.id = "invalid_" + attr;
+        p.setAttribute('style', "color: red; ");
+        p.textContent = "The field " + attr + " is not valid. ";
+
+        parent.appendChild(p);
+    }
+    if (v) {
+        validText[attr] = false;
+
+        var dom = document.getElementById("invalid_" + attr);
+        if (dom) dom.remove();
+    }
 }
 
-function pause() {
-    var pause = 'pause';
-    if(status.startsWith(pause)){
-        status = status.slice(pause.length)
-    }else {
-        status = pause + status;
+function checkValidate(User) {
+    var res = true;
+
+    form_Attrs.forEach(a => {
+        showValidation(a, User[a]);
+        res = res && User[a];
+    });
+
+    var incl = ['male', 'female', 'other'].includes(User['gender']);
+    showValidation('gender', incl);
+    res = res && incl;
+
+    // simplified RFC 2822 standard
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    var recl = re.test(String(User['email'].toLowerCase()));
+    showValidation('email', recl);
+    res = res && recl;
+
+    return res;
+}
+
+function formSubmit(event) {
+    event.preventDefault();
+    var User = form_Attrs.reduce((acc, val) => {
+        acc[val] = event.currentTarget[val].value;
+        return acc;
+    }, {});
+    if (checkValidate(User)) {
+        data.push(User);
+        localStorage.setItem('data', JSON.stringify(data));
+        appendtable(User);
     }
 }
